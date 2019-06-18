e0d6c29a4960836265112b6ffa0cace9d496114b
g-csg <brian@420blaze.it>
2019-06-18 20:08:45 +0100

    i work now

--- generate_all.sh
+++ generate_all.sh
@@ -1,2 +1,4 @@
 # update me if you add sections :)
-
+# normally you could do something like `catd > all.md' but these have to be in order :(
+# there is probably a better approach than this:
+cat index.md phones.md consumer_electronics.md pcbuilding.md keyboards.md mice.md iems.md headphones.md audiosources.md microphones.md electronics.md tools.md flashlights.md > all.md
